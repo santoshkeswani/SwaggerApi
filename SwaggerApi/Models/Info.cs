65f091f33241b6b55dc6dc604d364b8a7871d1bc
santoshkeswani <77826649+santoshkeswani@users.noreply.github.com>
2021-01-29 14:47:43 +0530

    Add files via upload

---
+++ SwaggerApi/Models/Info.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerApi.Models
+{
+    public class Info
+    {
+        public string Version { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string TermsOfService { get; set; }
+    }
+}
